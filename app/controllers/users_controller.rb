425f022636ff3829b74fc1cd24f48c851bf5b4ac
Will Kaspar <Will@Aang.local>
2017-03-11 11:43:56 -0800

    new user form up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to root_path
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'users#index'
 
-  get 'users/new' to: 'users#new' as: 'new_user'
-
+  get 'users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
 
 end
