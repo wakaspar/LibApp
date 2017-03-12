5686854032814fea307f3a40e80ee0b90be2ad98
Will Kaspar <Will@Aang.local>
2017-03-11 20:11:38 -0800

    new save - user#create finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,14 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id', to: 'users#show', as: 'user'
   post '/users', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
 
 end
